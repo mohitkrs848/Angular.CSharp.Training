d06d78c60f60f797d2d81297fcf297b4c4db8d09
mohitkrs848 <kumar.mohit@solera.com>
2024-09-08 14:23:14 +0530

    hosting in IIS server

--- Angular.CSharp.Training/App_Start/WebApiConfig.cs
+++ Angular.CSharp.Training/App_Start/WebApiConfig.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace Angular.CSharp.Training.App_Start
 {
@@ -10,6 +11,10 @@ namespace Angular.CSharp.Training.App_Start
     {
         public static void Register(HttpConfiguration config)
         {
+            // Enable CORS for all origins, headers, and methods
+            var cors = new EnableCorsAttribute("*", "*", "*"); // You can specify domains here instead of "*"
+            config.EnableCors(cors);
+
             // Web API routes
             config.MapHttpAttributeRoutes();
 
